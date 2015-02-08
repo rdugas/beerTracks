35aee5eb3c4b50753232365ffcae3b3864bf5fef
Rob Dugas <rob.dugas@gmail.com>
2015-02-07 18:10:01 -0600

    sync up to beers endpoint, new image

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
         // zero out results if previous search has run
         $('.results').html('');
         // get the value of the tags the user submitted
-        var tags = $(this).find("input[name='tags']").val();
-        getUnanswered(tags);
+        var searchString = $(this).find("input[name='tags']").val();
+        getBeers(searchString);
     });
 
     $('.inspiration-getter').submit(function(event) {
@@ -18,35 +18,34 @@ $(document).ready(function() {
 
 // this function takes the question object returned by StackOverflow 
 // and creates new result to be appended to DOM
-var showQuestion = function(question) {
+var showBeer = function(beerResult) {
 
     // clone our result template code
     var result = $('.templates .question').clone();
 
     // Set the question properties in result
-    var questionElem = result.find('.question-text a');
-    questionElem.attr('href', question.link);
-    questionElem.text(question.title);
+    var beerElem = result.find('.question-text a');
+    beerElem.attr('href', beerResult.link);
+    beerElem.text(beerResult.name);
 
     // set the date asked property in result
     var asked = result.find('.asked-date');
     // var date = new Date(1000*question.creation_date);
-    var date = new Date(1000 * question.creation_date);
+    var date = new Date(1000 * beerResult.updated_at);
     asked.text(date.toString());
 
     // set the #views for question property in result
-    var viewed = result.find('.viewed');
-    viewed.text(question.view_count);
+    var abvValue = result.find('.viewed');
+    abvValue.text(beerResult.abv);
 
     // set some properties related to asker
-    var asker = result.find('.asker');
-    asker.html('<p>Name: <a target="_blank" href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
-        question.owner.display_name +
+    var breweryName = result.find('.asker');
+    breweryName.html('<p>Name: <a target="_blank" href=' + beerResult.brewery.name + ' >' +
+        beerResult.brewery.name +
         '</a>' +
         '</p>' +
-        '<p>Reputation: ' + question.owner.reputation + '</p>'
+        '<p>Reputation: ' + beerResult.brewery.name + '</p>'
     );
-
 
     return result;
 };
@@ -89,30 +88,31 @@ var showError = function(error) {
 
 // takes a string of semi-colon separated tags to be searched
 // for on StackOverflow
-var getUnanswered = function(tags) {
+
+
+var getBeers = function(searchQuery) {
 
     // the parameters we need to pass in our request to StackOverflow's API
     var request = {
-        tagged: tags,
-        site: 'stackoverflow',
-        order: 'desc',
-        sort: 'creation'
+        query: searchQuery,
+        order: 'name DESC'
     };
 
+    // http://api.openbeerdatabase.com/v1/beers.json?query=ale
     var result = $.ajax({
-            url: "http://api.stackexchange.com/2.2/questions/unanswered",
+            url: "http://api.openbeerdatabase.com/v1/beers.json",
             data: request,
             dataType: "jsonp",
             type: "GET",
         })
         .done(function(result) {
-            var searchResults = showSearchResults(request.tagged, result.items.length);
+            var searchResults = showSearchResults(request.query, result.beers.length);
 
             $('.search-results').html(searchResults);
 
-            $.each(result.items, function(i, item) {
-                var question = showQuestion(item);
-                $('.results').append(question);
+            $.each(result.beers, function(i, item) {
+                var beerResult = showBeer(item);
+                $('.results').append(beerResult);
             });
         })
         .fail(function(jqXHR, error, errorThrown) {
